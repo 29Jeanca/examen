ce96a05c3ab537ba8ac2859aea6f0f839e6f43e3
29Jeanca <jeancarlosbarberena29@gmail.com>
2025-05-19 13:54:33 -0600

    cambios

--- backend/examen_back/settings.py
+++ backend/examen_back/settings.py
@@ -67,17 +67,20 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'examen_back.wsgi.application'
+ALLOWED_HOSTS = ['*']  
 
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import dj_database_url
+import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Password validation
