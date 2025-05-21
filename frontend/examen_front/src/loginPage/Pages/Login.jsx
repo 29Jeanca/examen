fa7b05275c249484444fd15a3dfba715cf8e8ee6
29Jeanca <jeancarlosbarberena29@gmail.com>
2025-05-20 20:59:20 -0600

    Agrega Login.css

--- frontend/examen_front/src/loginPage/Pages/Login.jsx
+++ frontend/examen_front/src/loginPage/Pages/Login.jsx
@@ -1,7 +1,7 @@
 import Footer from '../Components/Footer';
 import FormLogin from '../Components/FormLogin';
 import Header from '../Components/Header';
-import '../styles/Login.css';
+import '../Styles/Login.css';
 const Login = () => {
     return(
         <>
