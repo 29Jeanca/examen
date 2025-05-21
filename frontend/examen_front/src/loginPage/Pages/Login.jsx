4d115f09ad0d20cd6e71887f93cd09003e4d43a8
29Jeanca <jeancarlosbarberena29@gmail.com>
2025-05-20 21:24:44 -0600

    Cambio de nombre

--- frontend/examen_front/src/loginPage/Pages/Login.jsx
+++ frontend/examen_front/src/loginPage/Pages/Login.jsx
@@ -1,7 +1,6 @@
 import Footer from '../Components/Footer';
 import FormLogin from '../Components/FormLogin';
 import Header from '../Components/Header';
-import '../Styles/Login.css';
 const Login = () => {
     return(
         <>
